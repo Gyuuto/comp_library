01edcf4a50e44528dbe47213f85d262887f6aed1
Gyuuto <gyuuto@gmail.com>
2020-11-29 05:57:24 +0900

    Modified to struct.

--- DataStructure/SegTree.cpp
+++ DataStructure/SegTree.cpp
@@ -1,22 +1,5 @@
-struct SegTree
+class SegTree
 {
-	int pow2;
-	int n;
-	vi v;
-
-	pii calc_upper_pow2 ( int x )
-	{
-		pii ret = mp(1, 0);
-		rep(i, 31){
-			if( x < ret.fs ){
-				ret.sc = i;
-				break;
-			}
-			ret.fs *= 2;
-		}
-		return ret;
-	}
-	
 	SegTree ( const int N, const int init_val = IINF )
 	{
 		auto p = calc_upper_pow2(N);
@@ -33,28 +16,10 @@ struct SegTree
 
 		add_impl(idx/2);
 	}
-	void add_impl ( int i )
-	{
-		if( i == 0 ) return;
-
-		v[i] = v[i*2] + v[i*2+1];
-		add_impl( i/2 );
-	}
 
 	int sum ( int l, int r )
 	{
 		return sum_impl(l, r, 1, 0, n);
-	}
-	int sum_impl( int a, int b, int idx, int l, int r )
-	{
-		if( r <= a || b <= l ) return 0;
-		if( a <= l && r <= b ) return v[idx];
-		else{
-			int yl = sum_impl(a, b, 2*idx, l, (l+r)/2);
-			int yr = sum_impl(a, b, 2*idx+1, (l+r)/2, r);
-
-			return yl+yr;
-		}
 	}
 
 	void print_tree ()
@@ -68,5 +33,42 @@ struct SegTree
 			cout << endl;
 		}
 	}
-};
 
+private:
+	int pow2;
+	int n;
+	vi v;
+
+	pii calc_upper_pow2 ( int x )
+	{
+		pii ret = mp(1, 0);
+		rep(i, 31){
+			if( x < ret.fs ){
+				ret.sc = i;
+				break;
+			}
+			ret.fs *= 2;
+		}
+		return ret;
+	}
+
+	void add_impl ( int i )
+	{
+		if( i == 0 ) return;
+
+		v[i] = v[i*2] + v[i*2+1];
+		add_impl( i/2 );
+	}
+
+	int sum_impl( int a, int b, int idx, int l, int r )
+	{
+		if( r <= a || b <= l ) return 0;
+		if( a <= l && r <= b ) return v[idx];
+		else{
+			int yl = sum_impl(a, b, 2*idx, l, (l+r)/2);
+			int yr = sum_impl(a, b, 2*idx+1, (l+r)/2, r);
+
+			return yl+yr;
+		}
+	}
+};
